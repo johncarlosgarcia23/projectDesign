416ea1e29f4713e17a16d1f6822beb598788b329
johncarlosgarcia23 <johncarlosgarcia23@gmail.com>
2026-01-14 13:34:57 +0800

    Delete backend directory

--- backend/models/Event.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const eventSchema = new mongoose.Schema({
-  batteryName: { type: String, required: true },
-  connectedAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.model("Event", eventSchema);
--- backend/routes/forecastRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getForecastData } from "../controllers/forecastController.js";
-
-const router = express.Router();
-
-router.get("/battery", getForecastData);
-
-export default router;
